5d856e924e679e51effc0ebd8487a9d4443af273
Igor Nesterenya <nesterione@gmail.com>
2015-04-17 16:08:42 +0300

    Add demo

---
+++ OOP_CSharp_Examples/TreeViewExample/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TreeViewExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tb_path = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(264, 241);
+            this.treeView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(457, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load all nodes by path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_loadDirectoryClick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(282, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(371, 211);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // tb_path
+            // 
+            this.tb_path.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_path.Location = new System.Drawing.Point(282, 16);
+            this.tb_path.Name = "tb_path";
+            this.tb_path.Size = new System.Drawing.Size(168, 20);
+            this.tb_path.TabIndex = 3;
+            this.tb_path.Text = "C:\\Java";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 265);
+            this.Controls.Add(this.tb_path);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "TreeViewDemo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox tb_path;
+    }
+}
+
---
+++ OOP_CSharp_Examples/TreeViewExample/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeViewExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private String lastSelectedPath = "";
+
+        private void btn_loadDirectoryClick(object sender, EventArgs e)
+        {
+            String path = tb_path.Text;
+            DirectoryInfo directoryInfo = new DirectoryInfo(@path);
+            if (directoryInfo.Exists)
+            {
+                
+                treeView1.Nodes.Clear();
+                
+                treeView1.AfterSelect += treeView1_AfterSelect;
+                BuildTree(directoryInfo, treeView1.Nodes);
+
+                // Open saved path
+                if (lastSelectedPath.Length>0)
+                {
+                    var path_list = lastSelectedPath.Split('\\').ToList();
+                    foreach (TreeNode node in treeView1.Nodes)
+                        if (node.Text == path_list[0])
+                            ExpandSavedPath(node, path_list);
+                }
+            }
+            else
+            {
+                MessageBox.Show(String.Format("Path {0} not exist",@path));
+            }
+        }
+
+        /// <summary>
+        /// Open all nodes by saved path
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="path"></param>
+        private void ExpandSavedPath(TreeNode node, List<string> path)
+        {
+            path.RemoveAt(0);
+            node.Expand();
+
+            if (path.Count == 0)
+            {
+                // Select last node
+                node.TreeView.SelectedNode = node;
+            }
+
+            foreach (TreeNode mynode in node.Nodes)
+            {
+                if (path.Count == 0)
+                    return;
+                if (mynode.Text == path[0])
+                    ExpandSavedPath(mynode, path);
+            }
+        }
+
+        private void BuildTree(DirectoryInfo directoryInfo, TreeNodeCollection addInMe)
+        {
+            TreeNode curNode = addInMe.Add(directoryInfo.Name);
+
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                curNode.Nodes.Add(file.FullName, file.Name);
+            }
+            foreach (DirectoryInfo subdir in directoryInfo.GetDirectories())
+            {
+                BuildTree(subdir, curNode.Nodes);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            lastSelectedPath = e.Node.FullPath;
+
+            if (e.Node.Name.EndsWith("txt") || e.Node.Name.EndsWith("bat"))
+            {
+                this.richTextBox1.Clear();
+                StreamReader reader = new StreamReader(e.Node.Name);
+                this.richTextBox1.Text = reader.ReadToEnd();
+                reader.Close();
+            }
+        }
+    }
+}
