8a11b8e6f741e723bc24b645fa2f2af5dc648ae3
FriidGe <oorreexx@gmail.com>
2015-05-25 20:18:45 +0300

    Орещенко Е.Н. ИТ-21

---
+++ GamesLabs/2015/IT-21/Sea Wars/Form1.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace Lab13
+{
+    public partial class Form1 : Form
+    {
+        int turn = 0, game = 0;
+
+        Random random = new Random();
+        Panel[,] player, enemy;
+        Field playerField, enemyField;
+
+        int fieldSize;
+        int bX, bY;
+
+        bool flag = true;
+
+        string buttonText;
+        string winnerText;
+        string loserText;
+
+        public Form1()
+        {    
+            try
+            {
+                if (System.IO.File.Exists("xsd.xsd"))
+                {
+                    XmlReaderSettings xmlreadersettings = new XmlReaderSettings();
+                    xmlreadersettings.Schemas.Add(null, "xsd.xsd");
+                    xmlreadersettings.ValidationType = ValidationType.Schema;
+                    xmlreadersettings.ValidationEventHandler += new ValidationEventHandler(xmlreadersettingsValidationEventHandler);
+
+                    XmlReader xmlreader = XmlReader.Create("Settings.xml", xmlreadersettings);
+                    while (xmlreader.Read()) { }
+                }
+                else
+                {
+                    MessageBox.Show("Игра загружена со стандартными настройками!");
+                    throw new System.IO.FileNotFoundException();
+                }
+
+                if (flag)
+                {
+                    XMLWithDOM xmlWorker = new XMLWithDOM("Settings.xml");
+                    Config config = xmlWorker.GetConfig();
+                    fieldSize = config.FieldSize;
+                    bX = config.ButtonCoords.X;
+                    bY = config.ButtonCoords.Y;
+
+                    switch(config.Language)
+                    {
+                        case "rus":
+                            {
+                                buttonText = "Рестарт";
+                                winnerText = "Игра окончена, вы выиграли.";
+                                loserText = "Игра окончена, вы проиграли.";
+                                break;
+                            }
+                        case "eng":
+                            {
+                                buttonText = "Restart";
+                                winnerText = "Game over, you won.";
+                                loserText = "Game over, you lose.";
+                                break;
+                            }
+                    }
+                }
+                else
+                    throw new Exception();
+            }
+            catch
+            {
+                fieldSize = 10;
+                bX = 230;
+                bY = 50;
+
+                buttonText = "Restart";
+                winnerText = "Game over, you won.";
+                loserText = "Game over, you lose.";
+            }
+
+            player = new Panel[fieldSize, fieldSize];
+            enemy = new Panel[fieldSize, fieldSize];
+
+            InitializeComponent();
+
+            restart.Text = buttonText;
+            restart.Location = new Point(bX, bY);
+
+            playerField = new Field(random.Next(0, Int32.MaxValue), fieldSize);
+            enemyField = new Field(random.Next(0, Int32.MaxValue), fieldSize);
+
+            AddFields();
+            Reset();
+        }
+
+        void xmlreadersettingsValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+            {
+                MessageBox.Show("Внимание: " + e.Message);
+                flag = false;
+            }
+            else if (e.Severity == XmlSeverityType.Error)
+            {
+                MessageBox.Show("Ошибка: " + e.Message);
+                flag = false;
+            }
+        }
+
+        private void Check()
+        {
+            bool gameOver = true;
+
+            foreach (Ship s in playerField.Ships)
+            {
+                for (int i = s.Coordinates.Start.X; i <= s.Coordinates.End.X; ++i)
+                    for (int j = s.Coordinates.Start.Y; j <= s.Coordinates.End.Y; ++j)
+                        if ((playerField.Matrix[i, j] != 3))
+                            gameOver = false;
+            }
+
+            if (gameOver)
+            {
+                game = 1;
+                MessageBox.Show(loserText);
+            }
+
+            gameOver = true;
+
+            foreach (Ship s in enemyField.Ships)
+            {
+                for (int i = s.Coordinates.Start.X; i <= s.Coordinates.End.X; ++i)
+                    for (int j = s.Coordinates.Start.Y; j <= s.Coordinates.End.Y; ++j)
+                        if ((enemyField.Matrix[i, j] != 3))
+                            gameOver = false;
+            }
+
+            if (gameOver)
+            {
+                game = 1;
+                MessageBox.Show(winnerText);
+            }
+        }
+        private void EnemyMove()
+        {
+            if (game == 0)
+            {
+                int m, n;
+                bool flag = false;
+
+                while (!flag || (turn == 1))
+                {
+                    m = random.Next(0, (int)Math.Sqrt(player.Length));
+                    n = random.Next(0, (int)Math.Sqrt(player.Length));
+
+                    flag = false;
+
+                    if (playerField.Matrix[m, n] < 2)
+                    {
+                        flag = true;
+
+                        if (playerField.Matrix[m, n] == 0)
+                            playerField.Matrix[m, n] = 5;
+                        else
+                            playerField.Action(m, n);
+
+                        if (!((playerField.Matrix[m, n] == 2) || (playerField.Matrix[n, n] == 3)))
+                            turn = 0;
+
+                        Check();
+                    }
+                }
+            }
+        }
+        private void AddFields()
+        {
+            int top = 20,
+                left = this.DisplayRectangle.Width - 200 - 20;
+
+            Point location = new Point();
+
+            for (int i = 0; i < Math.Sqrt(player.Length); ++i)
+            {
+                left = this.DisplayRectangle.Width - 200 - 20;
+
+                for (int j = 0; j < Math.Sqrt(player.Length); ++j)
+                {
+                    location.X = left;
+                    location.Y = top;
+
+                    player[i, j] = new Panel();
+
+                    player[i, j].BackColor = SystemColors.ActiveCaption;
+                    player[i, j].Location = location;
+                    player[i, j].Width = player[i, j].Height = 20;
+                    player[i, j].BorderStyle = BorderStyle.Fixed3D;
+                    player[i, j].MouseHover += new EventHandler(panel_MouseHover);
+
+                    this.Controls.Add(player[i, j]);
+
+                    left += 20;
+                }
+                top += 20;
+            }
+
+            top = 20;
+            left = 20;
+
+            for (int i = 0; i < Math.Sqrt(enemy.Length); ++i)
+            {
+                left = 20;
+
+                for (int j = 0; j < Math.Sqrt(enemy.Length); ++j)
+                {
+                    location.X = left;
+                    location.Y = top;
+
+                    enemy[i, j] = new Panel();
+                    enemy[i, j].BackColor = SystemColors.ActiveCaption;
+                    enemy[i, j].Location = location;
+                    enemy[i, j].Width = enemy[i, j].Height = 20;
+                    enemy[i, j].BorderStyle = BorderStyle.Fixed3D;
+                    enemy[i, j].MouseHover += new EventHandler(panel_MouseHover);
+                    enemy[i, j].Click += new EventHandler(panel_Click);
+
+                    this.Controls.Add(enemy[i, j]);
+
+                    left += 20;
+                }
+
+                top += 20;
+            }
+        }
+        private void Redraw()
+        {
+            for (int i = 0; i < Math.Sqrt(player.Length); ++i)
+            {
+                for (int j = 0; j < Math.Sqrt(player.Length); ++j)
+                {
+                    switch (playerField.Matrix[i, j])
+                    {
+                        case 0:
+                            player[i, j].BackColor = SystemColors.ActiveCaption;
+
+                            break;
+                        case 1:
+                            player[i, j].BackColor = Color.White;
+
+                            break;
+                        case 2:
+                            player[i, j].BackColor = Color.Orange;
+
+                            break;
+                        case 3:
+                            player[i, j].BackColor = Color.DarkRed;
+
+                            break;
+                        case 4:
+                            player[i, j].BackColor = Color.Tan;
+
+                            break;
+                        case 5:
+                            player[i, j].BackColor = SystemColors.GrayText;
+
+                            break;
+                    }
+
+                    switch (enemyField.Matrix[i, j])
+                    {
+                        case 0:
+                            enemy[i, j].BackColor = SystemColors.ActiveCaption;
+
+                            break;
+                        //case 1:
+                        //    enemy[i, j].BackColor = Color.White;
+
+                        //    break;
+                        case 2:
+                            enemy[i, j].BackColor = Color.Orange;
+
+                            break;
+                        case 3:
+                            enemy[i, j].BackColor = Color.DarkRed;
+
+                            break;
+                        case 4:
+                            enemy[i, j].BackColor = Color.Tan;
+
+                            break;
+                        case 5:
+                            enemy[i, j].BackColor = SystemColors.GrayText;
+
+                            break;
+                    }
+                }
+            }
+        }
+        private void Reset()
+        {
+            index.Text = "0:0";
+
+            turn = 0;
+            game = 0;
+
+            playerField.Clear();
+            enemyField.Clear();
+
+            Redraw();
+
+            playerField.AddShips();
+            enemyField.AddShips();
+
+            Redraw();
+        }
+
+        private void panel_MouseHover(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Math.Sqrt(player.Length); ++i)
+            {
+                for (int j = 0; j < Math.Sqrt(player.Length); ++j)
+                {
+                    if ((player[i, j] == (sender as Panel)) || (enemy[i, j] == (sender as Panel)))
+                    {
+                        index.Text = string.Format("{0}:{1}", j + 1, i + 1);
+
+                        return;
+                    }
+                }
+            }
+        }
+        private void panel_Click(object sender, EventArgs e)
+        {
+            if (game == 0)
+            {
+                if (turn == 0)
+                {
+                    for (int i = 0; i < Math.Sqrt(player.Length); ++i)
+                    {
+                        for (int j = 0; j < Math.Sqrt(player.Length); ++j)
+                        {
+                            if (enemy[i, j] == (sender as Panel))
+                            {
+                                if (enemyField.Matrix[i, j] < 2)
+                                {
+                                    if (enemyField.Matrix[i, j] == 0)
+                                        enemyField.Matrix[i, j] = 5;
+                                    else
+                                        enemyField.Action(i, j);
+
+                                    if (!((enemyField.Matrix[i, j] == 2) || (enemyField.Matrix[i, j] == 3)))
+                                    {
+                                        turn = 1;
+
+                                        EnemyMove();
+                                    }
+
+                                    Redraw();
+                                    Check();
+                                }
+
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void restart_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+    }
+
+    class XMLWithDOM
+    {
+        private XmlDocument document;
+        public XMLWithDOM(String pathToXMLFile)
+        {
+            try
+            {
+                if (!(System.IO.File.Exists(pathToXMLFile)))
+                    throw new System.IO.FileNotFoundException();
+
+                document = new XmlDocument();
+                using (XmlReader reader = XmlReader.Create(pathToXMLFile))
+                {
+                    document.Load(reader);
+                }
+            }
+            catch { throw; }
+        }
+
+        private const String ROOT = "config";
+
+        public Config GetConfig()
+        {
+            XmlElement root = document[ROOT];
+
+            int x = int.Parse(root["buttonCoords"].FirstChild.InnerText);
+            int y = int.Parse(root["buttonCoords"].LastChild.InnerText);
+
+            int fieldsize = int.Parse(root["fieldSize"].InnerText);
+
+            string lang = root["language"].InnerText;
+
+            RestartButtonCoords coords = new RestartButtonCoords(x, y);
+            Config config = new Config(coords, fieldsize, lang);
+
+            return config;
+        }
+    }
+
+    class RestartButtonCoords
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public RestartButtonCoords(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    class Config
+    {
+        public RestartButtonCoords ButtonCoords { get; set; }
+        public int FieldSize { get; set; }
+        public string Language;
+
+        public Config(RestartButtonCoords coords, int fs, string lang)
+        {
+            ButtonCoords = coords;
+            FieldSize = fs;
+            Language = lang;
+        }
+
+        public override string ToString()
+        {
+            String viewTemplate = "Button coords: \n\tx:{0}\n\ty:{1}\nField Size: {2}";
+            return String.Format(viewTemplate, ButtonCoords.X, ButtonCoords.Y, FieldSize);
+        }
+    }
+}
---
+++ GamesLabs/2015/IT-21/Sea Wars/Logic.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab13
+{
+    class MyPoint
+    {
+        public int X, Y;
+    }
+    class Coordinates
+    {
+        public MyPoint Start;
+        public MyPoint End;
+
+        public Coordinates()
+        {
+            this.Start = new MyPoint();
+            this.End = new MyPoint();
+        }
+    }
+    class Border
+    {
+        public MyPoint Start;
+        public MyPoint End;
+
+        public Border()
+        {
+            this.Start = new MyPoint();
+            this.End = new MyPoint();
+        }
+    }
+
+    class Ship
+    {
+        public int Length;
+        public int Orientation;
+        public Border Border;
+        public Coordinates Coordinates;
+
+        public Ship()
+        {
+            this.Coordinates = new Coordinates();
+            this.Border = new Border();
+        }
+
+        public void BorderArea()
+        {
+            this.Border.Start.X = this.Coordinates.Start.X - 1;
+            this.Border.Start.Y = this.Coordinates.Start.Y - 1;
+
+            this.Border.End.X = this.Coordinates.End.X + 1;
+            this.Border.End.Y = this.Coordinates.End.Y + 1;
+
+            if (this.Coordinates.Start.X == 0)
+                this.Border.Start.X = 0;
+            else if (this.Coordinates.End.X == 9)
+                this.Border.End.X = 9;
+
+            if (this.Coordinates.Start.Y == 0)
+                this.Border.Start.Y = 0;
+            else if (this.Coordinates.End.Y == 9)
+                this.Border.End.Y = 9;
+        }
+        public void ResetPosition(int o, int x, int y)
+        {
+            this.Orientation = o;
+
+            switch (this.Orientation)
+            {
+                case 0:
+                    this.Coordinates.Start.X = x;
+                    this.Coordinates.Start.Y = y;
+
+                    this.Coordinates.End.X = this.Coordinates.Start.X + this.Length - 1;
+                    this.Coordinates.End.Y = this.Coordinates.Start.Y;
+
+                    break;
+                case 1:
+                    this.Coordinates.Start.X = y;
+                    this.Coordinates.Start.Y = x;
+
+                    this.Coordinates.End.X = this.Coordinates.Start.X;
+                    this.Coordinates.End.Y = this.Coordinates.Start.Y + this.Length - 1;
+
+                    break;
+            }
+
+            this.BorderArea();
+        }
+    }
+    class Field
+    {
+        public int[,] Matrix;
+        public List<Ship> Ships;
+        private Random Random;
+        private bool HasShip;
+        private int Killed;
+        private int Size;
+
+        public Field(int randomSeed, int size)
+        {
+            this.Matrix = new int[size, size];
+            this.Ships = new List<Ship>();
+            this.Random = new Random(randomSeed);
+            this.Size = size;
+
+            this.Clear();
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < Size; ++i)
+                for (int j = 0; j < Size; ++j)
+                    this.Matrix[i, j] = 0;
+
+            this.Ships.Clear();
+        }
+        public void SetupShips()
+        {
+            for (int i = 0; i < Size; ++i)
+                this.Ships.Add(new Ship());
+
+            Ships[0].Length = 4;
+
+            for (int i = 1; i < 3; ++i)
+                Ships[i].Length = 3;
+
+            for (int i = 3; i < 6; ++i)
+                Ships[i].Length = 2;
+
+            for (int i = 6; i < 10; ++i)
+                Ships[i].Length = 1;
+
+            for (int i = 0; i < 10; ++i)
+                Ships[i].ResetPosition(Random.Next(0, 2), Random.Next(0, 10 - Ships[i].Length + 1), Random.Next(0, 10));
+
+        }
+        public void AddShips()
+        {
+            SetupShips();
+
+            for (int n = 0; n < 10; ++n)
+            {
+                HasShip = false;
+
+                for (int i = Ships[n].Border.Start.X; i <= Ships[n].Border.End.X; ++i)
+                    for (int j = Ships[n].Border.Start.Y; j <= Ships[n].Border.End.Y; ++j)
+                        if (Matrix[i, j] == 1)
+                            HasShip = true;
+
+                if (!HasShip)
+                {
+                    for (int i = Ships[n].Coordinates.Start.X; i <= Ships[n].Coordinates.End.X; ++i)
+                        for (int j = Ships[n].Coordinates.Start.Y; j <= Ships[n].Coordinates.End.Y; ++j)
+                            Matrix[i, j] = 1;
+                }
+                else
+                {
+                    Ships[n].ResetPosition(Random.Next(0, 2), Random.Next(0, 10 - Ships[n].Length + 1), Random.Next(0, 10));
+
+                    --n;
+                }
+            }
+        }
+        public void Action(int x, int y)
+        {
+            if (this.Matrix[x, y] == 1)
+                this.Matrix[x, y] = 2;
+
+            foreach (Ship s in this.Ships)
+            {
+                this.Killed = 0;
+
+                for (int i = s.Coordinates.Start.X; i <= s.Coordinates.End.X; ++i)
+                    for (int j = s.Coordinates.Start.Y; j <= s.Coordinates.End.Y; ++j)
+                        if (this.Matrix[i, j] == 2)
+                            ++this.Killed;
+
+                if (this.Killed == s.Length)
+                {
+                    for (int i = s.Border.Start.X; i <= s.Border.End.X; ++i)
+                        for (int j = s.Border.Start.Y; j <= s.Border.End.Y; ++j)
+                            this.Matrix[i, j] = 4;
+
+                    for (int i = s.Coordinates.Start.X; i <= s.Coordinates.End.X; ++i)
+                        for (int j = s.Coordinates.Start.Y; j <= s.Coordinates.End.Y; ++j)
+                            this.Matrix[i, j] = 3;
+                }
+            }
+        }
+    }
+}
