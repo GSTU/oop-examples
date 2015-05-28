8f61703a1854ea43ffec44963652b906625c952f
yurii2004 <VIT19691994@mail.ru>
2015-05-28 12:44:40 +0300

    BelEvg IT-21

---
+++ GamesLabs/2015/IT-21/15-ki/Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace _15_ki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b15 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b14 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b11 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b10 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Memo = new System.Windows.Forms.RichTextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.minute = new System.Windows.Forms.Label();
+            this.seconds = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.OFD1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(18, 250);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(54, 20);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.Text = "129";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(18, 276);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(54, 20);
+            this.textBox2.TabIndex = 17;
+            this.textBox2.Text = "88";
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(129, 47);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(35, 35);
+            this.b8.TabIndex = 7;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b15
+            // 
+            this.b15.Location = new System.Drawing.Point(88, 129);
+            this.b15.Name = "b15";
+            this.b15.Size = new System.Drawing.Size(35, 35);
+            this.b15.TabIndex = 14;
+            this.b15.UseVisualStyleBackColor = true;
+            this.b15.Click += new System.EventHandler(this.b15_Click);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(6, 6);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(35, 35);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click_1);
+            // 
+            // b14
+            // 
+            this.b14.Location = new System.Drawing.Point(47, 129);
+            this.b14.Name = "b14";
+            this.b14.Size = new System.Drawing.Size(35, 35);
+            this.b14.TabIndex = 13;
+            this.b14.UseVisualStyleBackColor = true;
+            this.b14.Click += new System.EventHandler(this.b14_Click);
+            // 
+            // b13
+            // 
+            this.b13.Location = new System.Drawing.Point(6, 129);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(35, 35);
+            this.b13.TabIndex = 12;
+            this.b13.UseVisualStyleBackColor = true;
+            this.b13.Click += new System.EventHandler(this.b13_Click);
+            // 
+            // b12
+            // 
+            this.b12.Location = new System.Drawing.Point(129, 129);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(35, 35);
+            this.b12.TabIndex = 11;
+            this.b12.UseVisualStyleBackColor = true;
+            this.b12.Click += new System.EventHandler(this.b12_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(129, 6);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(35, 35);
+            this.b4.TabIndex = 3;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b11
+            // 
+            this.b11.Location = new System.Drawing.Point(88, 88);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(35, 35);
+            this.b11.TabIndex = 10;
+            this.b11.UseVisualStyleBackColor = true;
+            this.b11.Click += new System.EventHandler(this.b11_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(6, 47);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(35, 35);
+            this.b5.TabIndex = 4;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b10
+            // 
+            this.b10.Location = new System.Drawing.Point(47, 88);
+            this.b10.Name = "b10";
+            this.b10.Size = new System.Drawing.Size(35, 35);
+            this.b10.TabIndex = 9;
+            this.b10.UseVisualStyleBackColor = true;
+            this.b10.Click += new System.EventHandler(this.b10_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(47, 47);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(35, 35);
+            this.b6.TabIndex = 5;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(6, 88);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(35, 35);
+            this.b9.TabIndex = 8;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(88, 47);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(35, 35);
+            this.b7.TabIndex = 6;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.b6);
+            this.panel1.Controls.Add(this.b8);
+            this.panel1.Controls.Add(this.b11);
+            this.panel1.Controls.Add(this.b15);
+            this.panel1.Controls.Add(this.b5);
+            this.panel1.Controls.Add(this.b10);
+            this.panel1.Controls.Add(this.b1);
+            this.panel1.Controls.Add(this.b12);
+            this.panel1.Controls.Add(this.b3);
+            this.panel1.Controls.Add(this.b14);
+            this.panel1.Controls.Add(this.b9);
+            this.panel1.Controls.Add(this.b4);
+            this.panel1.Controls.Add(this.b13);
+            this.panel1.Controls.Add(this.b2);
+            this.panel1.Controls.Add(this.b7);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(172, 169);
+            this.panel1.TabIndex = 18;
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(88, 6);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(35, 35);
+            this.b3.TabIndex = 2;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(47, 6);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(35, 35);
+            this.b2.TabIndex = 1;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(190, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 35);
+            this.button1.TabIndex = 19;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Memo
+            // 
+            this.Memo.Location = new System.Drawing.Point(255, 18);
+            this.Memo.Name = "Memo";
+            this.Memo.Size = new System.Drawing.Size(145, 163);
+            this.Memo.TabIndex = 20;
+            this.Memo.Text = "";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(192, 89);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(33, 20);
+            this.textBox3.TabIndex = 21;
+            this.textBox3.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(187, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Ход";
+            // 
+            // minute
+            // 
+            this.minute.AutoSize = true;
+            this.minute.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.minute.ForeColor = System.Drawing.Color.Crimson;
+            this.minute.Location = new System.Drawing.Point(186, 133);
+            this.minute.Name = "minute";
+            this.minute.Size = new System.Drawing.Size(18, 20);
+            this.minute.TabIndex = 27;
+            this.minute.Text = "1";
+            // 
+            // seconds
+            // 
+            this.seconds.AutoSize = true;
+            this.seconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.seconds.ForeColor = System.Drawing.Color.Crimson;
+            this.seconds.Location = new System.Drawing.Point(207, 133);
+            this.seconds.Name = "seconds";
+            this.seconds.Size = new System.Drawing.Size(27, 20);
+            this.seconds.TabIndex = 28;
+            this.seconds.Text = "40";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.IndianRed;
+            this.label2.Location = new System.Drawing.Point(200, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 20);
+            this.label2.TabIndex = 29;
+            this.label2.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Meiryo UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label3.Location = new System.Drawing.Point(185, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 14);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Время:";
+            // 
+            // OFD1
+            // 
+            this.OFD1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(235, 187);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.seconds);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.minute);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Memo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Пятнашки";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b15;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b14;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b10;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox Memo;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label minute;
+        private System.Windows.Forms.Label seconds;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog OFD1;
+    }
+}
+
