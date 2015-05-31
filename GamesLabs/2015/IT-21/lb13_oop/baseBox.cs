ab8f3c46a3de83aea9c4ddb54e8831d3442a2601
tarasfromgomel <tarasfromgomel@gmail.com>
2015-05-31 21:23:42 +0300

    Taras Shevchnko add lb13

---
+++ GamesLabs/2015/IT-21/lb13_oop/baseBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace lb13_oop
+{
+    class baseBox
+    {
+        protected float x, y, w, h;
+        protected Color color;
+        public float X { get { return x; } }
+        public float Y { get { return y; } }
+        public float W { get { return w; } }
+        public float H { get { return h; } }
+
+
+        public baseBox(float x, float y, float w, float h)
+        {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+            color = Color.White;
+
+        }
+
+        virtual public void draw(PaintEventArgs e) {
+            color = Color.White;
+            e.Graphics.FillRectangle(new SolidBrush(color), x, y, w, h);
+            color = Color.Black;
+            e.Graphics.DrawRectangle(new Pen(color, 2), x, y, w, h);
+        }
+    }
+}
