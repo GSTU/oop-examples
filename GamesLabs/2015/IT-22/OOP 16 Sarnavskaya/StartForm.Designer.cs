31ce21f29c4f006b7fb920c5a8a535d1fa9a862b
sarnavskaya <t_sarnavskaya@mail.ru>
2015-06-01 21:50:29 +0300

    Сарнавская Татьяна ИТ-22

---
+++ GamesLabs/2015/IT-22/OOP 16 Sarnavskaya/StartForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartForm));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(283, 226);
+            this.Name = "StartForm";
+            this.Text = "StartForm";
+            this.Load += new System.EventHandler(this.StartForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ GamesLabs/2015/IT-22/OOP 16 Sarnavskaya/StartForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class StartForm : Form
+    {
+        public StartForm(int x, int y)
+        {
+            InitializeComponent();
+
+            this.Width = x;
+            this.Height = y;
+        }
+
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            Close();
+
+        }
+
+        private void StartForm_Load(object sender, EventArgs e)
+        {
+            
+            timer1.Start();
+        }
+    }
+}
