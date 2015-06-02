e2bd2349ed622f4e220ccb159298e94d7e2d166e
CrazyArchie <lapitskiy.a.a@gmail.com>
2015-06-02 10:05:04 +0300

    LA Морской бой

---
+++ GamesLabs/2015/IT-22/SF/Laba16/Cell.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Drawing;
+
+namespace Laba13
+{
+    class Cell
+    {
+        int x, y;
+        int number = -1;
+        SolidBrush brush;
+
+        public Cell(int x, int y, int number, string fieldColor)
+        {
+            this.x = x;
+            this.y = y;
+            this.number = number;
+            brush = new SolidBrush(ColorTranslator.FromHtml(fieldColor));
+        }
+
+        //Свойства доступа
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public SolidBrush SolidBrush
+        {
+            get { return brush; }
+        }
+    }
+}
---
+++ GamesLabs/2015/IT-22/SF/Laba16/PlayForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Laba13
+{
+    partial class PlayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayForm));
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.pictureBoxLeft = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRight = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnExit.BackgroundImage")));
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(607, 3);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(20, 20);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.BackColor = System.Drawing.Color.Transparent;
+            this.btnMin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMin.BackgroundImage")));
+            this.btnMin.FlatAppearance.BorderSize = 0;
+            this.btnMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMin.Location = new System.Drawing.Point(585, 3);
+            this.btnMin.Margin = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(20, 20);
+            this.btnMin.TabIndex = 3;
+            this.btnMin.UseVisualStyleBackColor = false;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // pictureBoxLeft
+            // 
+            this.pictureBoxLeft.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxLeft.Location = new System.Drawing.Point(3, 25);
+            this.pictureBoxLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBoxLeft.Name = "pictureBoxLeft";
+            this.pictureBoxLeft.Size = new System.Drawing.Size(311, 311);
+            this.pictureBoxLeft.TabIndex = 4;
+            this.pictureBoxLeft.TabStop = false;
+            this.pictureBoxLeft.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxLeft_Paint);
+            this.pictureBoxLeft.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBoxLeft_MouseClick);
+            // 
+            // pictureBoxRight
+            // 
+            this.pictureBoxRight.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxRight.Location = new System.Drawing.Point(316, 25);
+            this.pictureBoxRight.Margin = new System.Windows.Forms.Padding(2, 0, 0, 0);
+            this.pictureBoxRight.Name = "pictureBoxRight";
+            this.pictureBoxRight.Size = new System.Drawing.Size(311, 311);
+            this.pictureBoxRight.TabIndex = 5;
+            this.pictureBoxRight.TabStop = false;
+            this.pictureBoxRight.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxRight_Paint);
+            this.pictureBoxRight.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBoxRight_MouseClick);
+            // 
+            // PlayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(630, 339);
+            this.Controls.Add(this.pictureBoxRight);
+            this.Controls.Add(this.pictureBoxLeft);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btnExit);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(630, 339);
+            this.MinimumSize = new System.Drawing.Size(630, 339);
+            this.Name = "PlayForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PlayForm";
+            this.Load += new System.EventHandler(this.PlayForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PlayForm_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PlayForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PlayForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PlayForm_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.PictureBox pictureBoxLeft;
+        private System.Windows.Forms.PictureBox pictureBoxRight;
+    }
+}
+
