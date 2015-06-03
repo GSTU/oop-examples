104205bd886b4f41f991f358b3e3d53857797b9f
Sergey <xakerzerg@gmail.com>
2015-06-03 21:59:05 +0300

    Privalov Sergey IT-22

---
+++ GamesLabs/2015/IT-22/Zmeyka/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Laba_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Point = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(380, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Point
+            // 
+            this.Point.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Point.AutoSize = true;
+            this.Point.Location = new System.Drawing.Point(12, 521);
+            this.Point.Name = "Point";
+            this.Point.Size = new System.Drawing.Size(40, 13);
+            this.Point.TabIndex = 1;
+            this.Point.Text = "Point 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(922, 543);
+            this.Controls.Add(this.Point);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Point;
+    }
+}
+
---
+++ GamesLabs/2015/IT-22/Zmeyka/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace Laba_5
+{
+    public partial class Form1 : Form
+    {
+        Keys choose;
+        Timer timer;
+        Brush br;
+        Random rand = new Random();
+        static bool flag1 = true;
+        int x1 = 0,
+            y1 = 0;
+        int x = 10
+           , y = 10;
+        public int X1
+        {
+            get { return x1; }
+            set { x1 = value; }
+        }
+        public int Y1
+        {
+            get { return y1; }
+            set { y1 = value; }
+        }
+        public Form1()
+        {
+            br = Brushes.Red;
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+                timer = new Timer();
+                X = 10;
+                Y = 10;
+                XmlReaderSettings xmlreadersettings = new XmlReaderSettings();
+                xmlreadersettings.Schemas.Add(null, "xsd.xsd");
+                xmlreadersettings.ValidationType = ValidationType.Schema;
+                xmlreadersettings.ValidationEventHandler += new ValidationEventHandler(xmlreadersettingsValidationEventHandler);
+                XmlReader xmlreader = XmlReader.Create("xml.xml", xmlreadersettings);
+                while (xmlreader.Read()) { }
+                if (flag1 == true)
+                {
+                    XmlDocument xmldoc = new XmlDocument();
+                    xmldoc.Load("xml.xml");
+                    Color color = Color.FromName(xmldoc.DocumentElement.SelectSingleNode("color").InnerText);
+                    SolidBrush sbr = new SolidBrush(color);
+                    br = sbr;
+                    X = Convert.ToInt32(xmldoc.DocumentElement.SelectSingleNode("Position").FirstChild.InnerText);
+                    Y = Convert.ToInt32(xmldoc.DocumentElement.SelectSingleNode("Position").LastChild.InnerText);
+                    timer.Interval = Convert.ToInt32(xmldoc.DocumentElement.SelectSingleNode("tick").InnerText);
+                }
+
+            Graphics g = CreateGraphics();
+            button1.Visible = false;
+            //button1.Location = this.PointToClient(MousePosition);
+            g.Clear(Color.DarkGray);
+            
+            X1 = 0;
+            Y1 = 0;
+            CountPoint = 0;
+            level = 0;
+            choose = Keys.K;
+            ChangeMove = 0;
+            timer.Interval = 100;
+            timer.Start();
+            timer.Tick += new EventHandler(timer_Tick);
+        }
+
+        static void xmlreadersettingsValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+            {
+                MessageBox.Show("Внимание: " + e.Message);
+                flag1 = false;
+            }
+            else if (e.Severity == XmlSeverityType.Error)
+            {
+                MessageBox.Show("Ошибка: " + e.Message);
+                flag1 = false;
+            }
+        }
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        int changemove = 5;
+        public int ChangeMove
+        {
+            get { return changemove; }
+            set { changemove = value; }
+        }
+        int CountPoint = 0;
+        int level = 0;
+        Rectangle rect;
+        bool flag = true;
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            Graphics g1 = CreateGraphics();
+            g1.Clear(Color.DarkGray);
+            //------------------------------------------------
+            if (choose == Keys.Right)
+            {
+                if (ChangeMove == 3)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Boom noob!,try again");
+                    button1.Visible = true;
+                }
+                X += 10;
+                changemove = 0;
+            }
+            if (choose == Keys.Down)
+            {
+                if (ChangeMove == 2)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Boom noob!,try again");
+                    button1.Visible = true;
+                }
+                Y += 10;
+                changemove = 1;
+            }
+            if (choose == Keys.Up)
+            {
+                if (ChangeMove == 1)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Boom noob!,try again");
+                    button1.Visible = true;
+                }
+                Y -= 10;
+                changemove = 2;
+            }
+            if (choose == Keys.Left)
+            {
+                if (ChangeMove == 0)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Boom noob!,try again");
+                    button1.Visible = true;
+                }
+                X -= 10;
+                changemove = 3;
+            }
+           //int yc = MousePosition.X;
+           //int xc = MousePosition.Y;
+            //------------------------------------------------
+            if (X > 938 || Y > 581 || Y < 0 || X < 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Boom noob!,try again");
+                button1.Visible = true;
+            }
+            //-------------------------------------------------
+            if (( X >= X1 - 12 && Y >= Y1 - 12) && (X <= X1 + 12 && Y <= Y1 + 12) )
+            {
+                flag = true;
+                if (CountPoint == 2)
+                {
+                    CountPoint = 0;
+                    level++;
+                    if (timer.Interval == 20)
+                    {
+                        timer.Stop();
+                        MessageBox.Show("Congratulations son,you win");
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        timer.Interval -= 20;
+                    } 
+                }
+                Point.Text = "Point=" + CountPoint + "   " + "Level=" + level;
+                CountPoint++;
+
+            }
+            if (flag)
+            {
+                To4ka();
+                flag = false;
+            }
+            //----------------------------------------------------------
+            g1.DrawRectangle(Pens.Red, rect = new Rectangle(X1, Y1, 6, 6));
+            g1.FillRectangle(Brushes.Blue, rect);
+            g1.DrawRectangle(Pens.Red, rect = new Rectangle(X, Y, 16, 16));
+            g1.FillRectangle(br, rect);
+            //-------------------------------------------------------------------------
+            if (ChangeMove == 1)
+            {
+                g1.DrawRectangle(Pens.Black, rect = new Rectangle(X + 3, Y - 10, 10, 10));
+                g1.FillRectangle(Brushes.Black, rect);
+            }
+            if (ChangeMove == 0)
+            {
+                g1.DrawRectangle(Pens.Black, rect = new Rectangle(X - 10, Y + 3, 10, 10));
+                g1.FillRectangle(Brushes.Black, rect);
+            }
+            if (ChangeMove == 3)
+            {
+                g1.DrawRectangle(Pens.Black, rect = new Rectangle(X + 16, Y + 3, 10, 10));
+                g1.FillRectangle(Brushes.Black, rect);
+            }
+            if (ChangeMove == 2)
+            {
+                g1.DrawRectangle(Pens.Black, rect = new Rectangle(X + 3, Y + 16, 10, 10));
+                g1.FillRectangle(Brushes.Black, rect);
+            }
+            //----------------------------------------------------------------------
+        }
+        public void To4ka()
+        {
+            X1 = rand.Next(0, 938 - 50);
+            Y1 = rand.Next(0, 581 - 50);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            choose = e.KeyCode;
+        }
+
+
+        /*static bool flag1 = true;
+        public void ReadXML()
+        {
+            XmlReaderSettings xmlreadersettings = new XmlReaderSettings();
+            xmlreadersettings.Schemas.Add(null, "xsd.xsd");
+            xmlreadersettings.ValidationType = ValidationType.Schema;
+            xmlreadersettings.ValidationEventHandler += new ValidationEventHandler(xmlreadersettingsValidationEventHandler);
+            XmlReader xmlreader = XmlReader.Create("xml.xml", xmlreadersettings);
+            while (xmlreader.Read()) { }
+            if (flag1 == true)
+            {
+                Text = "Puzzle";
+                    label2.Text = "Лучший результат: ";
+                    label2.Visible = false;
+                    XmlDocument xmldoc = new XmlDocument();
+                    xmldoc.Load("xml.xml");
+                    bitmap = new Bitmap(xmldoc.DocumentElement.SelectSingleNode("path").InnerText);
+                    wlength = Convert.ToInt32(xmldoc.DocumentElement.SelectSingleNode("size").FirstChild.InnerText);
+                    hlength = Convert.ToInt32(xmldoc.DocumentElement.SelectSingleNode("size").LastChild.InnerText);
+                    NewGame();
+                    Text += " -> Уровень: " + xmldoc.DocumentElement.SelectSingleNode("level").InnerText;
+                    label2.Text += xmldoc.DocumentElement.SelectSingleNode("result").InnerText;
+                    label2.Visible = true;
+                    for (int i = 0; i < picturebox.Length; i++)
+                    {
+                        picturebox[i].Enabled = false;
+                    }
+                    восстановитьКартинкуToolStripMenuItem.Enabled = false;
+                    перемешатьКартинкуToolStripMenuItem.Enabled = true;
+                    помощьToolStripMenuItem.Enabled = true;
+                    настройкаСложностиToolStripMenuItem.Enabled = true;
+                    index = -1;
+            }
+            
+        }
+        static void xmlreadersettingsValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+            {
+                MessageBox.Show("Внимание: " + e.Message);
+                flag1 = false;
+            }
+            else if (e.Severity == XmlSeverityType.Error)
+            {
+                MessageBox.Show("Ошибка: " + e.Message);
+                flag1 = false;
+            }
+        }*/
+    }
+}
