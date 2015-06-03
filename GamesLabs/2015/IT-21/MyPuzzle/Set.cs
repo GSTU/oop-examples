fb92cc7c4a82641d4903a17427016e2939f83b99
Anastasia-Rybak <anastasia.rubak@yandex.by>
2015-06-03 15:18:47 +0300

    Anastasia Rybak IT-21

---
+++ GamesLabs/2015/IT-21/MyPuzzle/Set.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    public partial class Set : Form
+    {
+        public Set()
+        {
+            InitializeComponent();
+        }
+        public int wlength, hlength;
+        public bool flag = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wlength = Convert.ToInt32(numericUpDown1.Value);
+            hlength = Convert.ToInt32(numericUpDown2.Value);
+            flag = true;
+            Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
