c26a8f913815553930885fcf6ebda587cd83d7eb
Necromant1k <Necromant1k96@gmail.com>
2015-06-04 15:28:07 +0300

    Гузюк Андрей ИТ-21

---
+++ GamesLabs/2015/IT-21/Flappy Bird/Logic.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Flappy_Bird
+{
+    public class WindowSettings
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public WindowSettings()
+        {
+            this.Width = 616;
+            this.Height = 839;
+        }
+    }
+    public class PlayerSettings
+    {
+        public Point StartPosition { get; private set; }
+        public int Size { get; private set; }
+        public double Speed { get; private set; }
+        public double MaxVSpeed { get; private set; }
+        public double JumpHeight { get; private set; }
+
+        public PlayerSettings(int ps)
+        {
+            this.StartPosition = new Point(200, 300);
+            this.Size = ps;
+            this.Speed = 2.8;
+            this.MaxVSpeed = 10;
+            this.JumpHeight = -3.2;
+        }
+    }
+    public class WallsSettings
+    {
+        public int MinHeight { get; private set; }
+        public int Thickness { get; private set; }
+        public int Step { get; private set; }
+        public int Count { get; private set; }
+        public int HoleSize { get; private set; }
+
+        public WallsSettings(Settings settings, int hs)
+        {
+            this.MinHeight = 100;
+            this.Thickness = 80;
+            this.Step = this.Thickness * 2;
+            this.Count = (settings.WindowSettings.Width / (this.Thickness + this.Step)) + 1;
+            this.HoleSize = hs;
+        }
+    }
+    public class Settings
+    {
+        public WindowSettings WindowSettings { get; private set; }
+        public PlayerSettings PlayerSettings { get; private set; }
+        public WallsSettings WallsSettings { get; private set; }
+
+        public int GroundLevel { get; private set; }
+        public double Gravity { get; private set; }
+
+        public Settings(int gl, int hs, int ps)
+        {
+            this.WindowSettings = new WindowSettings();
+            this.PlayerSettings = new PlayerSettings(ps);
+            this.WallsSettings = new WallsSettings(this, hs);
+
+            this.GroundLevel = gl;
+            this.Gravity = 0.09;
+        }
+    }
+
+    public class Player
+    {
+        public Grid Body { get; private set; }
+        public Thickness Margin;
+        public SolidColorBrush Brush { get; private set; }
+        public double VSpeed;
+
+        private Random Random;
+
+        public Player(Settings settings, int seed)
+        {
+            this.Random = new Random(seed);
+
+            this.Body = new Grid();
+            this.Margin = new Thickness();
+            this.Brush = new SolidColorBrush();
+            this.VSpeed = 0;
+
+            this.Body.Width = this.Body.Height = settings.PlayerSettings.Size;
+            this.Body.Margin = this.Margin;
+            this.Body.Background = this.Brush;
+            this.Body.HorizontalAlignment = HorizontalAlignment.Left;
+            this.Body.VerticalAlignment = VerticalAlignment.Top;
+
+            Reset(settings);
+        }
+
+        public void Move(Settings settings)
+        {
+            this.VSpeed += settings.Gravity;
+            this.Margin.Top += this.VSpeed;
+
+            this.Body.Margin = this.Margin;
+
+            if (this.VSpeed > settings.PlayerSettings.MaxVSpeed)
+                this.VSpeed = settings.PlayerSettings.MaxVSpeed;
+        }
+        public void Reset(Settings settings)
+        {
+            this.VSpeed = 0;
+
+            this.Margin.Left = settings.PlayerSettings.StartPosition.X;
+            this.Margin.Top = settings.PlayerSettings.StartPosition.Y;
+            this.Margin.Right = 0;
+            this.Margin.Bottom = 0;
+
+            this.Body.Margin = this.Margin;
+
+            this.SetColor();
+        }
+        private void SetColor()
+        {
+            this.Brush.Color = Color.FromRgb((byte)Random.Next(0, 256), (byte)Random.Next(0, 256), (byte)Random.Next(0, 256));
+        }
+    }
+    public class WallBody
+    {
+        public Grid UpperBody { get; private set; }
+        public Grid LowerBody { get; private set; }
+        public Thickness UpperMargin;
+        public Thickness LowerMargin;
+        public SolidColorBrush Brush { get; private set; }
+
+        private int Number;
+        private Random Random;
+
+        public WallBody(Settings settings, int n, int seed)
+        {
+            this.Number = n;
+            this.Random = new Random(seed);
+
+            this.UpperBody = new Grid();
+            this.LowerBody = new Grid();
+            this.UpperMargin = new Thickness();
+            this.LowerMargin = new Thickness();
+            this.Brush = new SolidColorBrush();
+
+            this.UpperBody.Width = this.LowerBody.Width = settings.WallsSettings.Thickness;
+            this.UpperBody.Background = this.LowerBody.Background = this.Brush;
+            this.UpperBody.HorizontalAlignment = this.LowerBody.HorizontalAlignment = HorizontalAlignment.Left;
+            this.UpperBody.VerticalAlignment = VerticalAlignment.Stretch;
+            this.LowerBody.VerticalAlignment = VerticalAlignment.Stretch;
+
+            this.UpperMargin.Top = 0;
+            this.LowerMargin.Bottom = settings.GroundLevel;
+        }
+
+        public void SetColor()
+        {
+            this.Brush.Color = Color.FromRgb((byte)Random.Next(0, 256), (byte)Random.Next(0, 256), (byte)Random.Next(0, 256));
+        }
+        public void SetPosition(Settings settings)
+        {
+            this.UpperMargin.Left = this.LowerMargin.Left = (settings.WindowSettings.Width - 16) + this.Number * (settings.WallsSettings.Thickness + settings.WallsSettings.Step);
+            this.UpperMargin.Right = this.LowerMargin.Right = 0;
+        }
+        public void SetHole(Settings settings)
+        {
+            this.UpperMargin.Bottom = this.Random.Next(settings.WallsSettings.MinHeight + settings.GroundLevel + settings.WallsSettings.HoleSize, (settings.WindowSettings.Height - 39) - settings.WallsSettings.MinHeight + 1);
+            this.LowerMargin.Top = (settings.WindowSettings.Height - 39) - this.UpperMargin.Bottom + settings.WallsSettings.HoleSize;
+        }
+        public bool IsVisible()
+        {
+            if (this.UpperBody.Margin.Left + this.UpperBody.Width < 0)
+                return false;
+
+            return true;
+        }
+        public bool Compare(Player player, Settings settings, bool passed)
+        {
+            if (!passed && (player.Body.Margin.Left + settings.PlayerSettings.Size >= this.UpperBody.Margin.Left))
+            {
+                if ((player.Body.Margin.Top <= (settings.WindowSettings.Height - 39) - this.UpperBody.Margin.Bottom)
+                    ||
+                    (player.Body.Margin.Top + settings.PlayerSettings.Size >= this.LowerBody.Margin.Top))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+    public class Wall
+    {
+        public WallBody Body { get; private set; }
+        public bool Passed;
+
+        public Wall(Settings settings, int n, int seed)
+        {
+            this.Body = new WallBody(settings, n, seed);
+            this.Passed = false;
+
+            Reset(settings);
+        }
+
+        public void Move(Settings settings)
+        {
+            this.Body.UpperMargin.Left -= settings.PlayerSettings.Speed;
+            this.Body.UpperMargin.Right -= settings.PlayerSettings.Speed;
+
+            this.Body.LowerMargin.Left -= settings.PlayerSettings.Speed;
+            this.Body.LowerMargin.Right -= settings.PlayerSettings.Speed;
+
+            this.Body.UpperBody.Margin = this.Body.UpperMargin;
+            this.Body.LowerBody.Margin = this.Body.LowerMargin;
+        }
+        public void ShiftToEnd(Settings settings)
+        {
+            if (!this.Body.IsVisible())
+            {
+                this.Passed = false;
+
+                this.Body.UpperMargin.Left += settings.WallsSettings.Count * (settings.WallsSettings.Thickness + settings.WallsSettings.Step);
+                this.Body.UpperMargin.Right += settings.WallsSettings.Count * (settings.WallsSettings.Thickness + settings.WallsSettings.Step);
+
+                this.Body.LowerMargin.Left += settings.WallsSettings.Count * (settings.WallsSettings.Thickness + settings.WallsSettings.Step);
+                this.Body.LowerMargin.Right += settings.WallsSettings.Count * (settings.WallsSettings.Thickness + settings.WallsSettings.Step);
+
+                this.Body.UpperBody.Margin = this.Body.UpperMargin;
+                this.Body.LowerBody.Margin = this.Body.LowerMargin;
+
+                this.Body.SetHole(settings);
+                this.Body.SetColor();
+            }
+        }
+        public bool IsCollide(Player player, Settings settings)
+        {
+            return this.Body.Compare(player, settings, this.Passed);
+        }
+        public void Reset(Settings settings)
+        {
+            this.Passed = false;
+
+            this.Body.SetHole(settings);
+            this.Body.SetPosition(settings);
+            this.Body.SetColor();
+
+            this.Body.UpperBody.Margin = this.Body.UpperMargin;
+            this.Body.LowerBody.Margin = this.Body.LowerMargin;
+        }
+    }
+}
---
+++ GamesLabs/2015/IT-21/Flappy Bird/MainWindow.xaml.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace Flappy_Bird
+{
+    public partial class MainWindow : Window
+    {
+        Random Random;
+        DispatcherTimer Timer;
+
+        XMLConfig Config;
+        Settings Settings;
+        Player Player;
+        List<Wall> Walls;
+
+        bool Game;
+        bool Fail;
+        int Scores;
+
+        bool SettingsOK = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Startup();
+        }
+
+        private void ValidateXML(object sender, ValidationEventArgs e)
+        {
+            if ((e.Severity == XmlSeverityType.Warning) || (e.Severity == XmlSeverityType.Error))
+            {
+                MessageBox.Show("Ошибка настроек: " + e.Message);
+
+                SettingsOK = false;
+            }
+        }
+        private void Startup()
+        {
+            XmlReaderSettings xmlreadersettings = new XmlReaderSettings();
+            xmlreadersettings.Schemas.Add(null, "xsd.xsd");
+            xmlreadersettings.ValidationType = ValidationType.Schema;
+            xmlreadersettings.ValidationEventHandler += new ValidationEventHandler(ValidateXML);
+            XmlReader xmlreader = XmlReader.Create("Settings.xml", xmlreadersettings);
+            while (xmlreader.Read()) { }
+
+            try
+            {
+                if (SettingsOK)
+                {
+                    XMLWithDOM xmlWorker = new XMLWithDOM("Settings.xml");
+                    Config = xmlWorker.GetConfig();
+
+                    Settings = new Settings(Config.GroundLevel, Config.HoleSize, Config.PlayerSize);
+                }
+                else
+                    throw new Exception();
+            }
+            catch
+            {
+                Settings = new Settings(120, 140, 40);
+            }
+
+            Timer = new DispatcherTimer();
+            Timer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            Timer.Tick += new EventHandler(TimerTick);
+
+            Random = new Random();
+
+            this.Width = Settings.WindowSettings.Width;
+            this.Height = Settings.WindowSettings.Height;
+            this.scoresLabel.Height = Settings.GroundLevel;
+
+            Game = false;
+            Scores = 0;
+
+            CreateBackground();
+
+            Player = new Player(Settings, Random.Next(int.MaxValue));
+            Walls = new List<Wall>();
+
+            for (int i = 0; i < Settings.WallsSettings.Count; ++i)
+            {
+                Walls.Add(new Wall(Settings, i, Random.Next(int.MaxValue)));
+
+                mainGrid.Children.Add(Walls[i].Body.UpperBody);
+                mainGrid.Children.Add(Walls[i].Body.LowerBody);
+            }
+
+            mainGrid.Children.Add(Player.Body);
+        }
+        private void Reset()
+        {
+            Player.Reset(this.Settings);
+
+            for (int i = 0; i < Settings.WallsSettings.Count; ++i)
+                Walls[i].Reset(Settings);
+        }
+        private void CreateBackground()
+        {
+            Grid sky = new Grid();
+
+            sky.Background = Brushes.SkyBlue;
+            sky.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+            sky.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+            sky.Margin = new Thickness(0, 0, 0, Settings.GroundLevel);
+
+            mainGrid.Children.Add(sky);
+
+            Grid ground = new Grid();
+
+            ground.Background = Brushes.Moccasin;
+            ground.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+            ground.VerticalAlignment = System.Windows.VerticalAlignment.Bottom;
+            ground.Height = Settings.GroundLevel;
+
+            mainGrid.Children.Add(ground);
+        }
+
+        private void Click(object sender, MouseButtonEventArgs e)
+        {
+            if (!Game && !Fail)
+            {
+                this.Timer.Start();
+
+                Game = true;
+            }
+            else if (Game && !Fail)
+            {
+                Player.VSpeed = Settings.PlayerSettings.JumpHeight;
+            }
+            else if (!Game && Fail)
+            {
+                Player.Reset(Settings);
+
+                for (int i = 0; i < Settings.WallsSettings.Count; ++i)
+                    Walls[i].Reset(Settings);
+
+                Fail = false;
+                Scores = 0;
+                scoresLabel.Content = Scores;
+            }
+        }
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (Game && !Fail)
+            {
+                for (int i = 0; i < Settings.WallsSettings.Count; ++i)
+                {
+                    Walls[i].Move(Settings);
+                    Walls[i].ShiftToEnd(Settings);
+
+                    Player.Move(Settings);
+
+                    if (Walls[i].Body.UpperBody.Margin.Left + Settings.PlayerSettings.Size < Player.Margin.Left - Settings.PlayerSettings.Size)
+                    {
+                        if (!Walls[i].Passed)
+                            scoresLabel.Content = (++Scores).ToString();
+
+                        Walls[i].Passed = true;
+                    }
+                }
+            }
+            else if (!Game && Fail)
+            {
+                Player.VSpeed += 150 * Settings.Gravity;
+
+                Player.Move(Settings);
+            }
+
+            CheckHeight();
+            CheckCollisions();
+        }
+        private void CheckHeight()
+        {
+            if (Player.Body.Margin.Top < 0)
+            {
+                Player.Margin.Top = 0;
+                Player.Body.Margin = Player.Margin;
+            }
+            else if (Player.Body.Margin.Top + Settings.PlayerSettings.Size > (Settings.WindowSettings.Height - 39) - Settings.GroundLevel)
+            {
+                Player.Margin.Top = (Settings.WindowSettings.Height - 39) - Settings.GroundLevel - Settings.PlayerSettings.Size;
+                Player.Body.Margin = Player.Margin;
+
+                this.Timer.Stop();
+
+                Game = false;
+                Fail = true;
+            }
+        }
+        private void CheckCollisions()
+        {
+            for (int i = 0; i < Settings.WallsSettings.Count; ++i)
+            {
+                if (Walls[i].IsCollide(Player, Settings))
+                {
+                    Game = false;
+                    Fail = true;
+                }
+            }
+        }
+    }
+
+    class XMLWithDOM
+    {
+        private XmlDocument document;
+
+        public XMLWithDOM(String pathToXMLFile)
+        {
+            try
+            {
+                if (!(System.IO.File.Exists(pathToXMLFile)))
+                    throw new System.IO.FileNotFoundException();
+
+                document = new XmlDocument();
+
+                using (XmlReader reader = XmlReader.Create(pathToXMLFile))
+                {
+                    document.Load(reader);
+                }
+            }
+            catch { throw; }
+        }
+
+        public XMLConfig GetConfig()
+        {
+            XmlElement root = document["root"];
+
+            return new XMLConfig(int.Parse(root["sceneSettings"].FirstChild.InnerText),
+                                 int.Parse(root["gameplaySettings"].FirstChild.InnerText),
+                                 int.Parse(root["gameplaySettings"].LastChild.InnerText));
+        }
+    }
+    class XMLConfig
+    {
+        public int GroundLevel { get; set; }
+        public int HoleSize { get; set; }
+        public int PlayerSize { get; set; }
+
+        public XMLConfig(int gl, int hs, int ps)
+        {
+            this.GroundLevel = gl;
+            this.HoleSize = hs;
+            this.PlayerSize = ps;
+        }
+    }
+}
