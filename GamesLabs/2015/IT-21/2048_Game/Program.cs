557ad8773cc5a4cd19a3f17419378f538c15d2a7
Лешков Александр <shura.activ@yandex.ru>
2015-06-11 13:05:26 +0300

    2048 Выполнил: Лешков А.С.

---
+++ GamesLabs/2015/IT-21/2048_Game/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _2048_Game
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
