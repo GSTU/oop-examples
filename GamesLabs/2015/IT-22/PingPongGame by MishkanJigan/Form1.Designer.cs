fa3cc2339dd983a1752cdc054d02ac1c00e7766b
MishkanJigan <yakovkin.gr@gmail.com>
2015-06-13 23:16:38 +0300

    good job

---
+++ GamesLabs/2015/IT-22/PingPongGame by MishkanJigan/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace PingPongGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.stick2 = new System.Windows.Forms.PictureBox();
+            this.gameover_lbl = new System.Windows.Forms.Label();
+            this.points_lbl = new System.Windows.Forms.Label();
+            this.score_lbl = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.stick = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.stick2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stick)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.stick2);
+            this.playground.Controls.Add(this.gameover_lbl);
+            this.playground.Controls.Add(this.points_lbl);
+            this.playground.Controls.Add(this.score_lbl);
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.stick);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(984, 534);
+            this.playground.TabIndex = 0;
+            // 
+            // stick2
+            // 
+            this.stick2.BackColor = System.Drawing.Color.Black;
+            this.stick2.Location = new System.Drawing.Point(392, 31);
+            this.stick2.Name = "stick2";
+            this.stick2.Size = new System.Drawing.Size(200, 20);
+            this.stick2.TabIndex = 6;
+            this.stick2.TabStop = false;
+            // 
+            // gameover_lbl
+            // 
+            this.gameover_lbl.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.gameover_lbl.AutoSize = true;
+            this.gameover_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameover_lbl.Location = new System.Drawing.Point(350, 141);
+            this.gameover_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameover_lbl.Name = "gameover_lbl";
+            this.gameover_lbl.Size = new System.Drawing.Size(345, 220);
+            this.gameover_lbl.TabIndex = 5;
+            this.gameover_lbl.Text = "Game Over\r\n\r\nF1     - Restart\r\nEsc   - Exit";
+            // 
+            // points_lbl
+            // 
+            this.points_lbl.AutoSize = true;
+            this.points_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.points_lbl.Location = new System.Drawing.Point(216, 44);
+            this.points_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.points_lbl.Name = "points_lbl";
+            this.points_lbl.Size = new System.Drawing.Size(52, 55);
+            this.points_lbl.TabIndex = 4;
+            this.points_lbl.Text = "0";
+            this.points_lbl.Click += new System.EventHandler(this.point_lbl_Click);
+            // 
+            // score_lbl
+            // 
+            this.score_lbl.AutoSize = true;
+            this.score_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score_lbl.Location = new System.Drawing.Point(27, 44);
+            this.score_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.score_lbl.Name = "score_lbl";
+            this.score_lbl.Size = new System.Drawing.Size(169, 55);
+            this.score_lbl.TabIndex = 3;
+            this.score_lbl.Text = "Score:";
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(115, 102);
+            this.ball.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(29, 30);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // stick
+            // 
+            this.stick.BackColor = System.Drawing.Color.Black;
+            this.stick.Location = new System.Drawing.Point(392, 482);
+            this.stick.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.stick.Name = "stick";
+            this.stick.Size = new System.Drawing.Size(200, 20);
+            this.stick.TabIndex = 1;
+            this.stick.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 534);
+            this.Controls.Add(this.playground);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.stick2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stick)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox stick;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label points_lbl;
+        private System.Windows.Forms.Label score_lbl;
+        private System.Windows.Forms.Label gameover_lbl;
+        private System.Windows.Forms.PictureBox stick2;
+    }
+}
+
---
+++ GamesLabs/2015/IT-22/PingPongGame by MishkanJigan/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace PingPongGame
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 4;                    // начальная скорость
+        public int speed_top = 4;
+        public int points = 0;                        // начальный счет
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+            Cursor.Hide();                                             // прячим курсор
+
+            this.FormBorderStyle = FormBorderStyle.None;               // прячит другие границы
+            this.TopMost = true;                                       // на передний план
+            this.Bounds = Screen.PrimaryScreen.Bounds;                 // делает во весь экран 
+
+            stick.Top = playground.Bottom  - (playground.Bottom / 20); // устанавливает позицию "бревнышка"
+ 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            stick.Left = Cursor.Position.X - (stick.Width / 2);        // устанавливает по центру курсор на "бревнышке"
+            gameover_lbl.Visible = false;
+            ball.Left += speed_left;                                   // движения "мяча" 
+            ball.Top += speed_top;
+
+
+            if (ball.Bottom >= stick2.Top && ball.Bottom <= stick2.Bottom + 20 && ball.Left >= stick2.Left && ball.Right <= stick2.Right) // коллизии верхнего "бруска"
+            {
+                speed_top -= 1;
+                speed_left -= 1;
+                speed_top = -speed_top;
+                points += 1;
+                points_lbl.Text = points.ToString();
+
+                //Random r = new Random();
+                //playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255)); // рандомный фон после очередного набранного очка
+            }
+            
+
+            if (ball.Bottom <= stick2.Top)
+            
+            {
+                
+                timer1.Enabled = false;                      // остановить игру,когда мяч не в игре 
+                gameover_lbl.Visible = true;                 // показать меню
+
+            }
+
+            if (ball.Bottom >= stick.Top && ball.Bottom <= stick.Bottom && ball.Left >= stick.Left && ball.Right <= stick.Right) // коллизии нижниге "бруска"
+            {
+                speed_top += 1;
+                speed_left += 1;
+                speed_top = -speed_top;
+                points += 1;
+                points_lbl.Text = points.ToString();
+
+                //Random r = new Random();
+                //playground.BackColor = Color.FromArgb(r.Next(150,255),r.Next(150,255),r.Next(150,255)); // рандомный фон после очередного набранного очка
+            }
+
+            if(ball.Left <= playground.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Top <= playground.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if (ball.Bottom >= playground.Bottom)
+            {
+                timer1.Enabled = false;                      // остановить игру,когда мяч не в игре 
+                gameover_lbl.Visible = true;                 // показать меню               
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+
+        {
+            if (e.KeyCode == Keys.Right) { stick2.Left += 100; }   // движение верхнего "бруска" на кнопки
+            if (e.KeyCode == Keys.Left) { stick2.Left -= 100; }
+            if (e.KeyCode == Keys.A) { stick2.Left -= 75; }
+            if (e.KeyCode == Keys.D) { stick2.Left += 75; }
+            if (e.KeyCode == Keys.Escape) { this.Close();}         // выход из игры
+            if (e.KeyCode == Keys.F1)                              // перезапуск игры
+            {
+                ball.Top = 50;
+                ball.Left = 50;
+                speed_left = 4;
+                speed_top = 4;
+                points = 0;
+                points_lbl.Text = "0";
+                timer1.Enabled = true;
+                gameover_lbl.Visible = false;
+                playground.BackColor = Color.White;
+            }
+
+        }
+
+        private void point_lbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        static bool valid = true;
+
+        public static void ValidationHandler(object sender, ValidationEventArgs args)
+        {
+            MessageBox.Show(args.Severity + ":" + args.Message + "\nЗагружены стандартные параметры");
+            valid = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Загрузка стандартных компонентов
+            int score = 0;
+            string backColor = "#FF5F9EA0";
+            string b1 = "#FF0000FF";
+            string b2 = "#FFFFFF00";
+
+            XmlTextReader reader = new XmlTextReader("config.xml");
+            XmlValidatingReader validreader = new XmlValidatingReader(reader);
+            validreader.Schemas.Add(null, "configSchema.xsd");
+            validreader.ValidationType = ValidationType.Schema;
+            validreader.ValidationEventHandler += new ValidationEventHandler(ValidationHandler);
+
+            try
+            {
+                while (validreader.Read()) ;
+            }
+            catch
+            {
+                valid = false;
+                MessageBox.Show("Файл конфигурации не найден\nЗагружены параметры по умолчанию");
+            }
+            validreader.Close();
+
+            if (valid)
+            {
+                reader = new XmlTextReader("config.xml");
+                while (reader.Read())
+                {
+                    if (reader.Name == "backColor" && reader.NodeType != XmlNodeType.EndElement)
+                        backColor = reader.ReadElementContentAsString();
+                    if (reader.Name == "score" && reader.NodeType != XmlNodeType.EndElement)
+                        score = reader.ReadElementContentAsInt();
+                    if (reader.Name == "b1" && reader.NodeType != XmlNodeType.EndElement)
+                        b1 = reader.ReadElementContentAsString();
+                    if (reader.Name == "b2" && reader.NodeType != XmlNodeType.EndElement)
+                        b2 = reader.ReadElementContentAsString();
+                }
+                reader.Close();
+            }
+
+            points = score;
+            points_lbl.Text = points.ToString();
+
+            playground.BackColor = ColorTranslator.FromHtml(backColor);
+            stick.BackColor = ColorTranslator.FromHtml(b1);
+            stick2.BackColor = ColorTranslator.FromHtml(b2);
+        }
+    }
+}
